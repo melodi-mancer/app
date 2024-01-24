7ea2620376c9154e14107263a517e29eaf1202b6
Jake Jones <jacob.jones@valtech.com>
2024-01-24 14:48:54 +0100

    remove info text

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,22 +6,12 @@ const footer = (props) => {
     <footer>
       {logged ? <div className="logout"><button onClick={e => authHelpers.logout()}>&gt; log out</button></div> : <div className="logout"></div>}
       <div className="github"><button onClick={footer.goToRepo}></button></div>
-      <div className="info">
-        {/* <div className="spoti">Spotify API is a shy one, so bear with me.</div> */}
-        <div className="react">made with <h3 onClick={footer.goToReact}>React</h3>.. and some <h2>☕</h2></div>
-      </div>
-
     </footer>
   )
 }
 
-footer.goToReact = () => {
-  let url = 'https://reactjs.org'
-  window.open(url, '_blank');
-}
-
 footer.goToRepo = () => {
-  let url = 'https://github.com/mystictide/explore-spotify'
+  let url = 'https://github.com/melodi-mancer/app'
   window.open(url, '_blank');
 }
 
